0e020c48f85cdd30b163dec1fbf62a3e12d38963
- <ytw1122@gmail.com>
2019-08-01 08:54:50 +0000

    init

--- src/main.cpp
+++ src/main.cpp
@@ -20,10 +20,7 @@ unsigned long int pingTimer;
 float Kp = 2.5;                //P게인 값
 float Ki = 0;                  //I게인 값 
 float Kd = 1;                  //D게인 값
-double Setpoint, Input, Output, ServoOutput;                                       
-float duration, distance;
-
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);   //PID객체 생성
+    
 
 //모터
 Servo myServo;                                     //서보 객체 생성, 초기화
@@ -31,8 +28,6 @@ Servo m_left_front;
 Servo m_left_rear;
 Servo m_right_front;
 Servo m_right_rear;
-
-
 
 //센서들
 const int s_side_sonar1=1;
@@ -42,15 +37,24 @@ const int s_bottom_lazer2=4; //드론 높이 제어 센서
 const int s_front_lidar1=5;
 const int s_front_lidar2=6;
 
-int echoPin = 6;                 //초음파 핀번호 설정
-int trigPin = 7;
-
 float getHeight()
 {        
     int data = analogRead(s_bottom_lazer2); 
 	int volt = map(data, 0, 1023, 0, 5000);
-	distance = (21.61/(volt-0.1696))*1000; 
+	float distance = (21.61/(volt-0.1696))*1000; 
     return distance;
+}
+
+long getSonarDis(int TRIG,int ECHO)
+{
+    long dist;
+    digitalWrite(TRIG, LOW); 
+    delayMicroseconds(2); 
+    digitalWrite(TRIG, HIGH); 
+    delayMicroseconds(10);
+    digitalWrite(TRIG, LOW);
+    dist = pulseIn(ECHO, HIGH,DIST_S)/58.2;
+    return(dist);
 }
 
 float getLeftDis()
@@ -80,33 +84,9 @@ void roll()
     
 }
 
-
-float readPosition() {
-    delay(40);                             //딜레이 설정 
-
-  //초음파 센서 거리 측정 부분
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(2);
-    digitalWrite(trigPin, LOW);
-
-    duration = pulseIn(echoPin, HIGH); 
-    distance = ((float)(340 * duration) / 10000) / 2;  
-
-    if(distance > 30) {                   //공의 측정거리가 30cm 이상일 경우 최대 30으로 설정
-        distance=30;
-    }
-    Serial.println(distance);             //시리얼 모니터로 공의 거리 출력
-    return distance;                      //측정값 반환
-}
-
-
 void setup() {
     Serial.begin(38400);                  //시리얼 통신 초기화
     
-    myServo.attach(servoPin);            //서보모터 핀번호 설정
-
-    Input = readPosition();             //막대 위의 공의 위치를 측정값 함수 호출
-  
     pinMode(s_side_sonar1,OUTPUT);
     pinMode(s_side_sonar2,OUTPUT);
     pinMode(s_side_sonar2,INPUT);
@@ -114,33 +94,13 @@ void setup() {
 //                                      초음파 센서
 
     //초음파 센서 설정
-    pinMode(trigPin, OUTPUT);
-    pinMode(echoPin, INPUT);                                                       
     pingTimer=millis();
     
-
-    myPID.SetMode(AUTOMATIC);               //PID모드를 AUTOMATIC으로 설정
-    myPID.SetOutputLimits(-80,80);          //PID의 값을 최소 -80부터 최대 80까지 설정
 }
 
 
 void loop() {
-    init();
-    Setpoint = 15;                         //막대 중앙 위치(Set Point를 15cm로 설정)
-    Input = readPosition();                //공의 위치 측정                            
-    myPID.Compute();                       //PID계산 
-    ServoOutput=100+Output;            //서보모터의 각도 설정(100도는 서보모터가 수평을 이루었을 때 각도) 
-    myServo.write(ServoOutput);            //서보모터에게 값 전달
 }
 
-long trig_ultra(int TRIG,int ECHO)
-{
-    long dist;
-    digitalWrite(TRIG, LOW); 
-    delayMicroseconds(2); 
-    digitalWrite(TRIG, HIGH); 
-    delayMicroseconds(10);
-    digitalWrite(TRIG, LOW);
-    dist = pulseIn(ECHO, HIGH,DIST_S)/58.2;
-    return(dist);
-}
+//초음파 거리 측정
+
